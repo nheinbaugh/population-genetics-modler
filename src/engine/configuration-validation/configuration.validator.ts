a850e9a466055791a47beed07a50a31d89a96320
Nick Heinbaugh <nick.heinbaugh@live.com>
2026-06-01 15:04:39 -0700

    removed empty file and set up zed workspace config

--- src/engine/configuration-validation/configuration.validator.ts
+++
@@ -1,3 +0,0 @@
-import type { Allele } from "../../models/core/allele.interface";
-import type { Locus } from "../../models/core/locus.interface";
-import type { SimulationConfiguration } from "../../models/simulation/simulation.interface";
