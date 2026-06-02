e30a8b926270d62d03b7eec14ec18d125810347d
Nick Heinbaugh <nick.heinbaugh@live.com>
2026-06-02 12:27:32 -0700

    fixed test that was broken due to added feature for mutation matrix

--- src/engine/configuration-validation/validate-locus-mutation-matrix.functions.ts
+++ src/engine/configuration-validation/validate-locus-mutation-matrix.functions.ts
@@ -1,34 +1,33 @@
-import type { Allele } from "../../models/core/allele.interface";
 import type { Locus } from "../../models/core/locus.interface";
 import { ConfigurationValidationError } from "./configuration-validation.error";
 
 export function validateMutationAlleleIds(modeledLoci: Locus[]): void {
-	modeledLoci.forEach((locus) => {
-		const validAlleleIds = new Set(locus.alleles.map((a) => a.id));
+  modeledLoci.forEach((locus) => {
+    const validAlleleIds = new Set(locus.alleles.map((a) => a.id));
 
-		if (!locus.mutationMatrix || !locus.mutationMatrix.length) {
-			throw new ConfigurationValidationError(
-				`Locus with ID: ${locus.id} does not contain a mutation matrix'`,
-			);
-		}
-		locus.mutationMatrix.forEach((mutationConfig) => {
-			if (!validAlleleIds.has(mutationConfig.fromAlleleId)) {
-				throw new ConfigurationValidationError(
-					`Locus '${locus.label}' (ID: ${locus.id}) has a mutation configuration with an invalid sourceAlleleId: '${mutationConfig.fromAlleleId}'.`,
-				);
-			}
-			mutationConfig.mutationRates.forEach((mutationRate) => {
-				if (!validAlleleIds.has(mutationRate.toAlleleId)) {
-					throw new ConfigurationValidationError(
-						`Locus '${locus.label}' (ID: ${locus.id}) has a mutation configuration with an invalid mutatedAlleleId: '${mutationRate.toAlleleId}'.`,
-					);
-				}
-				if (mutationRate.rateOfMutation < 0) {
-					throw new ConfigurationValidationError(
-						`Locus '${locus.label}' has a mutation configuration with a negative mutation rate`,
-					);
-				}
-			});
-		});
-	});
+    if (!locus.mutationMatrix || !locus.mutationMatrix.length) {
+      throw new ConfigurationValidationError(
+        `Locus with ID: ${locus.id} does not contain a mutation matrix'`,
+      );
+    }
+    locus.mutationMatrix.forEach((mutationConfig) => {
+      if (!validAlleleIds.has(mutationConfig.fromAlleleId)) {
+        throw new ConfigurationValidationError(
+          `Locus '${locus.label}' (ID: ${locus.id}) has a mutation configuration with an invalid sourceAlleleId: '${mutationConfig.fromAlleleId}'.`,
+        );
+      }
+      mutationConfig.mutationRates.forEach((mutationRate) => {
+        if (!validAlleleIds.has(mutationRate.toAlleleId)) {
+          throw new ConfigurationValidationError(
+            `Locus '${locus.label}' (ID: ${locus.id}) has a mutation configuration with an invalid mutatedAlleleId: '${mutationRate.toAlleleId}'.`,
+          );
+        }
+        if (mutationRate.rateOfMutation < 0) {
+          throw new ConfigurationValidationError(
+            `Locus '${locus.label}' has a mutation configuration with a negative mutation rate`,
+          );
+        }
+      });
+    });
+  });
 }
--- src/engine/configuration.validator.spec.ts
+++ src/engine/configuration.validator.spec.ts
@@ -1,120 +1,128 @@
 import { describe, expect, test } from "vitest";
+
 import type { SimulationConfiguration } from "../models/simulation/simulation.interface";
 import { validateSimulationConfiguration } from "./configuration-validation";
 import { ConfigurationValidationError } from "./configuration-validation/configuration-validation.error";
 
 describe("validateSimulationConfiguration", () => {
-	describe("validateLocusExistance", () => {
-		test("it should throw an error when there are no alleles in a locus", () => {
-			const config: SimulationConfiguration = {
-				isPopulationSizeStatic: true,
-				initialPopulationSize: 200,
-				strategy: "population",
-				generations: 10,
-				modeledLoci: [],
-			};
-			expect(() => validateSimulationConfiguration(config)).toThrow();
-		});
-	});
+  describe("validateLocusExistance", () => {
+    test("it should throw an error when there are no alleles in a locus", () => {
+      const config: SimulationConfiguration = {
+        isPopulationSizeStatic: true,
+        initialPopulationSize: 200,
+        strategy: "population",
+        generations: 10,
+        modeledLoci: [],
+      };
+      expect(() => validateSimulationConfiguration(config)).toThrow();
+    });
+  });
 
-	describe("validateLocusFrequencies", () => {
-		test("it should throw an error when the sum of alleles for a locus is less than 1", () => {
-			const config: SimulationConfiguration = {
-				isPopulationSizeStatic: true,
-				initialPopulationSize: 200,
-				strategy: "population",
-				generations: 10,
-				modeledLoci: [
-					{
-						id: "locus|a",
-						label: "Basic Allele",
-						mutationMatrix: [],
-						alleles: [
-							{
-								id: "allele|a",
-								label: "a",
-								initialFrequency: 0.2,
-								fitnessModifier: 1,
-							},
-							{
-								id: "allele|b",
-								label: "b",
-								initialFrequency: 0.2,
-								fitnessModifier: 1,
-							},
-						],
-					},
-				],
-			};
-			expect(() => validateSimulationConfiguration(config)).toThrow(
-				ConfigurationValidationError,
-			);
-		});
+  describe("validateLocusFrequencies", () => {
+    test("it should throw an error when the sum of alleles for a locus is less than 1", () => {
+      const config: SimulationConfiguration = {
+        isPopulationSizeStatic: true,
+        initialPopulationSize: 200,
+        strategy: "population",
+        generations: 10,
+        modeledLoci: [
+          {
+            id: "locus|a",
+            label: "Basic Allele",
+            mutationMatrix: [],
+            alleles: [
+              {
+                id: "allele|a",
+                label: "a",
+                initialFrequency: 0.2,
+                fitnessModifier: 1,
+              },
+              {
+                id: "allele|b",
+                label: "b",
+                initialFrequency: 0.2,
+                fitnessModifier: 1,
+              },
+            ],
+          },
+        ],
+      };
+      expect(() => validateSimulationConfiguration(config)).toThrow(
+        ConfigurationValidationError,
+      );
+    });
 
-		test("it should throw an error when the sum of alleles for a locus is more than 1", () => {
-			const config: SimulationConfiguration = {
-				isPopulationSizeStatic: true,
-				initialPopulationSize: 200,
-				strategy: "population",
-				generations: 10,
-				modeledLoci: [
-					{
-						id: "locus|a",
-						label: "Basic Allele",
-						mutationMatrix: [],
-						alleles: [
-							{
-								id: "allele|a",
-								label: "a",
-								initialFrequency: 0.2,
-								fitnessModifier: 1,
-							},
-							{
-								id: "allele|b",
-								label: "b",
-								initialFrequency: 0.9,
-								fitnessModifier: 1,
-							},
-						],
-					},
-				],
-			};
-			expect(() => validateSimulationConfiguration(config)).toThrow(
-				ConfigurationValidationError,
-			);
-		});
+    test("it should throw an error when the sum of alleles for a locus is more than 1", () => {
+      const config: SimulationConfiguration = {
+        isPopulationSizeStatic: true,
+        initialPopulationSize: 200,
+        strategy: "population",
+        generations: 10,
+        modeledLoci: [
+          {
+            id: "locus|a",
+            label: "Basic Allele",
+            mutationMatrix: [],
+            alleles: [
+              {
+                id: "allele|a",
+                label: "a",
+                initialFrequency: 0.2,
+                fitnessModifier: 1,
+              },
+              {
+                id: "allele|b",
+                label: "b",
+                initialFrequency: 0.9,
+                fitnessModifier: 1,
+              },
+            ],
+          },
+        ],
+      };
+      expect(() => validateSimulationConfiguration(config)).toThrow(
+        ConfigurationValidationError,
+      );
+    });
 
-		test("it should not throw an error when the sum of alleles for a loucs is 1", () => {
-			const config: SimulationConfiguration = {
-				isPopulationSizeStatic: true,
-				initialPopulationSize: 200,
-				strategy: "population",
-				generations: 10,
-				modeledLoci: [
-					{
-						id: "locus|a",
-						label: "Basic Allele",
-						mutationMatrix: [],
-						alleles: [
-							{
-								id: "allele|a",
-								label: "a",
-								initialFrequency: 0.75,
-								fitnessModifier: 1,
-							},
-							{
-								id: "allele|b",
-								label: "b",
-								initialFrequency: 0.25,
-								fitnessModifier: 1,
-							},
-						],
-					},
-				],
-			};
-			expect(() => validateSimulationConfiguration(config)).not.toThrow(
-				ConfigurationValidationError,
-			);
-		});
-	});
+    test("it should not throw an error when the sum of alleles for a loucs is 1", () => {
+      const config: SimulationConfiguration = {
+        isPopulationSizeStatic: true,
+        initialPopulationSize: 200,
+        strategy: "population",
+        generations: 10,
+        modeledLoci: [
+          {
+            id: "locus|a",
+            label: "Basic Allele",
+            mutationMatrix: [
+              {
+                fromAlleleId: "allele|a",
+                mutationRates: [
+                  { toAlleleId: "allele|b", rateOfMutation: 0.01 },
+                ],
+              },
+            ],
+            alleles: [
+              {
+                id: "allele|a",
+                label: "a",
+                initialFrequency: 0.75,
+                fitnessModifier: 1,
+              },
+              {
+                id: "allele|b",
+                label: "b",
+                initialFrequency: 0.25,
+                fitnessModifier: 1,
+              },
+            ],
+          },
+        ],
+      };
+      expect(() => validateSimulationConfiguration(config)).not.toThrow(
+        ConfigurationValidationError,
+      );
+    });
+  });
 });
